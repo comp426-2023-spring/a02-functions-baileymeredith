ec95a56263127c18814b33ce34755a4d3eb165e2
Bailey DeSouza <bdesouza@email.unc.edu>
2023-02-23 16:48:12 -0500

    added some comments.

--- cli.js
+++ cli.js
@@ -5,7 +5,6 @@ import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 
 const args = minimist(process.argv.slice(2));
-// const help_message = `Usage: ./galo.sh [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n  -h            Show this help message and exit.\n  -n, -s        Latitude: N positive; S negative.\n  -e, -w        Longitude: E positive; W negative.\n  -z            Time zone: uses /etc/timezone by default.\n  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n  -v            Verbose output: returns full weather forecast.\n  -j            Echo pretty JSON from open-meteo API and exit.`
 const days = args.d
 const timezone = setTimeZone();
 const latitude = setLatitude();
